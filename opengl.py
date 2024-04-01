1d1bbe4022d07ed5633ca7b91f19296f45f81c1e
anton0909 <anton0909@naver.com>
2024-04-01 23:12:54 +0900

    On branch main Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	new file:   opengl.py

---
+++ opengl.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def draw_triangle():
+    glBegin(GL_TRIANGLES)
+    glVertex2f(-0.5, -0.5)
+    glVertex2f(0.5, -0.5)
+    glVertex2f(0.0, 0.5)
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        draw_triangle()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == "__main__":
+    main()
